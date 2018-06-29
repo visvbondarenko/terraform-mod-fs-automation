11e9d606c4e87dc979633b00bae34a3fdd087994
vladyslav.bondarenko <visvbondarenko@gmail.com>
2018-06-29 15:39:28 +0300

    test commit tags

--- 04-instances.tf
+++ 04-instances.tf
@@ -8,22 +8,22 @@ resource "aws_instance" "jenkins" {
   vpc_security_group_ids  = ["${aws_security_group.jenkins.id}", "${aws_security_group.tools.id}"]
   disable_api_termination = true
 
-  lifecycle {
-    ignore_changes = ["ami"]
-  }
-
-  tags {
-    Account_shorthand = "${var.account_shorthand}"
-    Name        = "${var.account_shorthand}_${var.environment}_${var.type}_${var.subtype_jenkins}${format("%02d", count.index + 1)}"
-    Environment = "${var.environment}"
-    Type        = "${var.type}"
-    Subtype     = "${var.subtype_jenkins}"
-    Identifier  = "${var.account_shorthand}_${var.environment}_${var.type}_${var.subtype_jenkins}"
-    ManagedBy   = "${var.mgmt}"
-    Service     = "${var.service}"
-    Owner       = ""
-    Project     = ""
-  }
+//  lifecycle {
+//    ignore_changes = ["ami"]
+//  }
+//
+//  tags {
+//    Account_shorthand = "${var.account_shorthand}"
+//    Name        = "${var.account_shorthand}_${var.environment}_${var.type}_${var.subtype_jenkins}${format("%02d", count.index + 1)}"
+//    Environment = "${var.environment}"
+//    Type        = "${var.type}"
+//    Subtype     = "${var.subtype_jenkins}"
+//    Identifier  = "${var.account_shorthand}_${var.environment}_${var.type}_${var.subtype_jenkins}"
+//    ManagedBy   = "${var.mgmt}"
+//    Service     = "${var.service}"
+//    Owner       = ""
+//    Project     = ""
+//  }
 }
 
 resource "aws_instance" "tools" {
@@ -35,20 +35,20 @@ resource "aws_instance" "tools" {
   vpc_security_group_ids  = ["${aws_security_group.jenkins.id}", "${aws_security_group.tools.id}"]
   disable_api_termination = true
 
-  lifecycle {
-    ignore_changes = ["ami"]
-  }
-
-  tags {
-    Account_shorthand = "${var.account_shorthand}"
-    Name        = "${var.account_shorthand}_${var.environment}_${var.type}_${var.subtype_tools}${format("%02d", count.index + 1)}"
-    Environment = "${var.environment}"
-    Type        = "${var.type}"
-    Subtype     = "${var.subtype_tools}"
-    Identifier  = "${var.account_shorthand}_${var.environment}_${var.type}_${var.subtype_tools}"
-    ManagedBy   = "${var.mgmt}"
-    Service     = "${var.service}"
-    Owner       = ""
-    Project     = ""
-  }
+//  lifecycle {
+//    ignore_changes = ["ami"]
+//  }
+//
+//  tags {
+//    Account_shorthand = "${var.account_shorthand}"
+//    Name        = "${var.account_shorthand}_${var.environment}_${var.type}_${var.subtype_tools}${format("%02d", count.index + 1)}"
+//    Environment = "${var.environment}"
+//    Type        = "${var.type}"
+//    Subtype     = "${var.subtype_tools}"
+//    Identifier  = "${var.account_shorthand}_${var.environment}_${var.type}_${var.subtype_tools}"
+//    ManagedBy   = "${var.mgmt}"
+//    Service     = "${var.service}"
+//    Owner       = ""
+//    Project     = ""
+//  }
 }
